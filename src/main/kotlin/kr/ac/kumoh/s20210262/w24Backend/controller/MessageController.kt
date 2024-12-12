bead5543b70bae66afed8f32150d09d698f22b29
둠팬 <dngur521@kumoh.ac.kr>
2024-12-12 14:04:19 +0900

    🚧 CORS 추가

--- src/main/kotlin/kr/ac/kumoh/s20210262/w24Backend/controller/MessageController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20210262/w24Backend/controller/MessageController.kt
@@ -2,15 +2,16 @@ package kr.ac.kumoh.s20210262.w24Backend.controller
 
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("/api/message")
+@CrossOrigin(origins = ["https://wsp2024hyeokfrontend.netlify.app/"])
+
 class MessageController {
     @GetMapping
-    // TODO: React 주소 변경할 것
-    @CrossOrigin(origins = ["http://localhost:3000"])
+    fun getRoot() = mapOf("status" to "success")
+
+    @GetMapping("/api/message")
     fun getMessage() = mapOf("status" to "success")
 }
